b7d37f6e91cf06a819c45b30c259314818b91dd6
Shlomo-kodkod <quarts.91.elders@icloud.com>
2025-09-07 16:11:21 +0300

    feat: add mongodb dal

--- services/config.py
+++ services/config.py
@@ -5,4 +5,7 @@ KAFKA_TOPIC = os.getenv("KAFKA_TOPIC", "metadata")
 KAFKA_KEY = os.getenv("KAFKA_TOPIC", "retriever")
 KAFKA_GROUP_ID = os.getenv("KAFKA_GROUP_ID", "persister")
 DATA_PATH = "podcasts"
+ES_HOST = os.getenv("ES_HOST", "localhost")
+ES_PORT = os.getenv("ES_PORT", 9200)
+ES_INDEX = os.getenv("ES_INDEX", "podcast_metadata")
 
---
+++ services/dal/mongo.py
@@ -0,0 +1,75 @@
+from pymongo import MongoClient
+from services.utils.utils import setup_logger
+
+
+class DAL:
+    _instance = None
+    _initialized = False
+
+    def __new__(cls, uri):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+    def __init__(self, uri):
+        if DAL._initialized:
+            return
+        self.__logger = setup_logger("DAL")
+        self.__uri = uri
+        self.__client = None
+        self.__db = None
+        DAL._initialized = True
+
+    def connect(self, db: str):
+        """
+        Connect to the mongo database.
+        """
+        if self.__db is not None:
+            self.__logger.info("Already connected to MongoDB")
+            return
+        try:
+            uri = self.__uri
+            self.__client = MongoClient(uri)
+            self.__db = self.__client[db]
+            self.__logger.info(f"Connected to MongoDB database: {db}")
+        except Exception as e:
+            self.__logger.error(f"Failed to connect to MongoDB: {e}")
+            raise e
+
+    def disconnect(self):
+        """
+        Disconnect from the mongo database.
+        """
+        if self.__client is not None:
+            self.__client.close()
+            self.__logger.info("Database connection closed")
+
+    def read_collection(self, collection_name: str, query: list[dict] = None):
+        """
+        Read a collection from the MongoDB database.
+        """
+        if query is None:
+            query = []
+        try:
+            collection = self.__db[collection_name]
+            data = list(collection.aggregate(query))
+            self.__logger.info(f"Data loaded successfully from: {collection_name}")
+            return data
+        except Exception as e:
+            self.__logger.error(f"Failed to retrieve collection : {e}")
+            raise e
+        
+    def create_document(self, collection_name: str, data: dict | list[dict], many: bool = False):
+        """
+        Create a document and insert into the mongodb collection.
+        """
+        try:
+            collection = self.__db[collection_name]
+            result = collection.insert_one(data) if not many and isinstance(data, dict) else collection.insert_many(data)
+            self.__logger.info(f"Data inserted successfully to: {collection_name}")
+            return result.acknowledged
+        except Exception as e:
+            self.__logger.error(f"Failed to insert data: {e}")
+            raise e
+
+    
