021e851a18f49f51fff6b5bda79443dc46b0e75d
Shlomo-kodkod <quarts.91.elders@icloud.com>
2025-09-08 09:10:37 +0300

    edit config name

--- services/config.py
+++ services/config.py
@@ -1,8 +1,8 @@
 import os
 
-BOOTSTRAP_SERVER = os.getenv("KAFKA_TOPIC", "localhost:9092")
+BOOTSTRAP_SERVER = os.getenv("BOOTSTRAP_SERVER", "localhost:9092")
 KAFKA_TOPIC = os.getenv("KAFKA_TOPIC", "metadata")
-KAFKA_KEY = os.getenv("KAFKA_TOPIC", "retriever")
+KAFKA_KEY = os.getenv("KAFKA_KEY", "retriever")
 KAFKA_GROUP_ID = os.getenv("KAFKA_GROUP_ID", "persister")
 DATA_PATH = "podcasts"
 ES_HOST = os.getenv("ES_HOST", "localhost")
